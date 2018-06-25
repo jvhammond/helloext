1524572f2f52a4943a3753b860eb2af2b747d8bc
Jeff Hammond <jefhammond@paypal.com>
2018-06-25 12:56:38 -0400

    adding character count command

--- src/extension.ts
+++ src/extension.ts
@@ -14,14 +14,28 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let hello = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!');
     });
 
-    context.subscriptions.push(disposable);
+    let nbrCharacters = vscode.commands.registerCommand('extension.nbrCharacters', () => {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        let selection = editor.selection;
+        let text = editor.document.getText(selection);
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Selected characters: ' + text.length);
+    });
+
+    context.subscriptions.push(hello);
+    context.subscriptions.push(nbrCharacters);
 }
 
 // this method is called when your extension is deactivated
